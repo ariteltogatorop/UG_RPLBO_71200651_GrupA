685009037869a47ba962163b4e2bf11b09386e31
ArielTogatorop <78332725+ariteltogatorop@users.noreply.github.com>
2022-09-08 18:33:11 +0700

    UG2_71200651

---
+++ UG2_71200651/2_71200651/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    Scanner inp = new Scanner (System.in);
+    System.out.println("Berapa karyawan: ");
+    int jmlLoop = inp.nextInt(); //jumlah karyawan yang ingin dicek gajinya
+    System.out.println("Masukan jumlah jam kerja: ");
+    int totJamKerja = inp.nextInt(); //total jam kerja karyawan
+    {
+        if (totJamKerja == 8 ) ;
+            inp
+
+    }
+}
