ca68d45c8d26a8b7a7d2c9fda08e0c7783ff8121
ariteltogatorop <joiner.tennye@ti.ukdw.ac.id>
2022-09-15 18:31:55 +0700

    UG3_71200651_Soal2

---
+++ UG3_71200651/Soal2_71200651/UG3_71200651_soal2/src/main/java/com/ug3/soal2/UG3soal2.java
@@ -0,0 +1,19 @@
+package com.ug3.soal2;
+import java.util.Scanner;
+
+
+public class UG3soal2
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner(System.in) ;
+        System.out.println( "Bilangan Fibonacci dari kata " );
+        System.out.println( "Masukkan input pertama " );
+        String pilihan1 = input.nextLine();
+        System.out.println( "Masukkan input kedua " );
+        String pilihan2 = input.nextLine();
+        System.out.println( "Bilangan Fibonacci" );
+
+
+    }
+}
