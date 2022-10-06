085f13df51ad8911a46b5c4e061ccb29467e1587
ariteltogatorop <78332725+ariteltogatorop@users.noreply.github.com>
2022-10-06 17:04:10 +0700

    -

---
+++ 71200651-UG6-1/src/main/java/com/ug6/soal1/Goods.java
@@ -0,0 +1,8 @@
+package com.ug6.soal1;
+
+public class Goods {
+    private String code ;
+    private String name ;
+    private int quantity ;
+    private double price ;
+}
---
+++ 71200651-UG6-1/src/main/java/com/ug6/soal1/vendingMachines.java
@@ -0,0 +1,9 @@
+package com.ug6.soal1;
+import java.util.ArrayList;
+
+public class vendingMachines {
+    private String code ;
+    private int capacity ;
+    private int usedCapacity ;
+    private goods []
+}
